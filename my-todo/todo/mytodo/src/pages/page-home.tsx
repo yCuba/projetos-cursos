291890ca0cd178d5e17a1639070841cb8099e7c3
Sergio Cuba <sergiohcompany@gmail.com>
2026-01-14 05:30:38 -0300

    Criando Componente de Tarefa

--- my-todo/todo/mytodo/src/pages/page-home.tsx
+++ my-todo/todo/mytodo/src/pages/page-home.tsx
@@ -1,4 +1,5 @@
 import Container from "../components/container";
+import TaskItem from "../core-components/task-item";
 import TasksSummary from "../core-components/tasks-summary";
 
 export default function PageHome() {
@@ -7,6 +8,9 @@ export default function PageHome() {
             <header className="flex items-center justify-between">
                 <TasksSummary />
             </header>
+
+
+            <TaskItem />
         </Container>
     );
 }
