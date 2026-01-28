44c13ed7894d4dccfb9b12c2bc31df9f5f74003d
Sergio Cuba <sergiohcompany@gmail.com>
2026-01-28 15:03:18 -0300

    implement game listing

---
+++ game-track/src/service/api.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+// PARTE 1: Configuração Base
+// Criamos uma instância do Axios com o endereço da RAWG.
+// Isso evita que você tenha que digitar o link inteiro toda vez.
+const api = axios.create({
+  baseURL: 'https://api.rawg.io/api',
+});
+
+// PARTE 2: A Função de Busca (Exportada para o App.tsx usar)
+export const getGames = async () => {
+  // O 'async' avisa que essa função pode demorar um pouco (esperando a internet).
+  try {
+    const response = await api.get('/games', {
+      params: {
+        // Buscamos a chave que você salvou no .env.local
+        key: import.meta.env.VITE_RAWG_API_KEY, 
+        page_size: 12, // Pedimos apenas 12 jogos para não travar
+      },
+    });
+
+    // Retornamos apenas a lista de jogos que está dentro de 'results'
+    return response.data.results;
+  } catch (error) {
+    console.error("Erro ao buscar jogos:", error);
+    return []; // Se der erro, retornamos uma lista vazia para o App não quebrar
+  }
+};
