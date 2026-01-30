e13e8e24c9fc08c56318abc1114eb4217cc815d3
Sergio Cuba <sergiohcompany@gmail.com>
2026-01-30 06:34:39 -0300

    style: search bar UI

--- game-track/src/App.tsx
+++ game-track/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { getGames } from "./service/api";
+import { Search } from "lucide-react";
 
 interface Game {
   id: number;
@@ -28,9 +29,10 @@ function App() {
 
   return (
     <div className="min-h-screen bg-[#0a0a0a] text-white p-8">
+      
       <header className="flex justify-between items-center mb-10">
         <h1 className="text-3xl font-bold bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent">
-          GameTrack
+          Games
         </h1>
         <div className="bg-gray-800 px-4 py-2 rounded-full text-sm font-medium">
           Minha Coleção
@@ -38,12 +40,20 @@ function App() {
       </header>
 
       <main>
-        <div className="mb-8">
+
+        <div className="mb-8 relative group">
+
+          <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 group-focus-within:text-purple-500 transition-colors"
+            size={20} 
+          />
+
           <input
             type="text"
             placeholder="Buscar jogos..."
-            className="w-full p-3 rounded-lg bg-gray-800 text-white border border-gray-700 focus:border-purple-500 outline-none transition-all"
+            value={search}
             onChange={(e) => setSearch(e.target.value)}
+            className="w-full py-3 pl-10 pr-3 rounded-lg bg-gray-800 text-white border border-gray-700 focus:border-purple-500 focus:ring-1 focus:ring-purple-500 outline-none transition-all"
+            
           />
         </div>
 
@@ -59,7 +69,7 @@ function App() {
   ) : (
     // PARTE B: Se não tiver jogos (lista vazia), mostra o seu card de loading
         <div className="bg-gray-900 rounded-xl animate-pulse p-4">
-          <h2 className="font-bold text-gray-500">Buscando jogos na RAWG...</h2>
+          <h2 className="font-bold text-gray-500">Buscando jogos...</h2>
         </div>
       )}
       </div>
