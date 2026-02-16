61760bbbe31e82c869cbbd20cf3fc792c53df7e2
Sergio Cuba <sergiohcompany@gmail.com>
2026-02-16 09:57:12 -0300

    Config prisma

--- login-guard/prisma.config.ts
+++ login-guard/prisma.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: "postgresql://admin:password123@localhost:5432/loginguard?schema=public",
   },
 });
