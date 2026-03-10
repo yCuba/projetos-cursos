90c19c2ad4ead24b986652ee62e64583d7e3aa20
Sergio Cuba <sergiohcompany@gmail.com>
2026-03-10 11:26:49 -0300

    Atualização Tarefas-Criadas / Concluídas

--- my-todo/todo/mytodo/src/core-components/tasks-summary.tsx
+++ my-todo/todo/mytodo/src/core-components/tasks-summary.tsx
@@ -1,17 +1,26 @@
 import Badge from "../components/badge";
 import Text from "../components/text";
+import useTasks from "../hooks/use-tasks";
 
 export default function TasksSummary() {
-    return (
-        <>
-            <div className="flex items-center gap-2">
-                <Text variant="body-sm-bold" className="!text-gray-300">Tarefa criadas</Text>
-                <Badge variant="secondary">5</Badge>
-            </div>
-            <div className="flex items-center gap-2">
-                <Text variant="body-sm-bold" className="!text-gray-300">Concluídas</Text>
-                <Badge variant="primary">2 de 5</Badge>
-            </div>
-        </>
-    );
+  const { createdTasksCount, concludedTasksCount } = useTasks();
+
+  return (
+    <>
+      <div className="flex items-center gap-2">
+        <Text variant="body-sm-bold" className="!text-gray-300">
+          Tarefa criadas
+        </Text>
+        <Badge variant="secondary">{createdTasksCount}</Badge>
+      </div>
+      <div className="flex items-center gap-2">
+        <Text variant="body-sm-bold" className="!text-gray-300">
+          Concluídas
+        </Text>
+        <Badge variant="primary">
+          {concludedTasksCount} de {createdTasksCount}
+        </Badge>
+      </div>
+    </>
+  );
 }
