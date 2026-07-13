71b2217fd43584e5aa6d6aa1f3a4e1e124afb186
Sergio Cuba <sergiohcompany@gmail.com>
2026-07-13 14:38:18 -0300

    feat(db): implement select and insert methods for in-memory database

--- my-server-nodejs/src/server.js
+++ my-server-nodejs/src/server.js
@@ -1,31 +1,35 @@
-import http from 'node:http'
-import { json } from './middlewares/json.js'
+import http from "node:http";
+import { json } from "./middlewares/json.js";
+import { Database } from "./database.js";
 
-const users = []
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
-  const { method, url } = req
+  const { method, url } = req;
 
-  await json(req, res)
+  await json(req, res);
 
-  if (method === 'GET' && url === '/users') {
-    return res
-      .end(JSON.stringify(users))
+  if (method === "GET" && url === "/users") {
+    const users = database.select("users");
+
+    return res.end(JSON.stringify(users));
   }
 
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
+  if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
 
-    users.push({
+    const user = {
       id: 1,
       name,
       email,
-    })
+    };
 
-    return res.writeHead(201).end()
+    database.insert("users", user);
+
+    return res.writeHead(201).end("201 Created");
   }
 
-  return res.writeHead(404).end()
-})
+  return res.writeHead(404).end();
+});
 
-server.listen(3333)
+server.listen(3333);
